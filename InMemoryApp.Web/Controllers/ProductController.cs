565287a3c9145146a22df65b47c71fc8a59d092c
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-03-13 16:08:53 +0300

    TryGetValue-Remove metodu

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -14,11 +14,31 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("zaman",DateTime.Now.ToString());
+            //zaman keyi var mı 1.yol
+            if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+            //2.yol
+            if (!_memoryCache.TryGetValue("zaman", out string zamancache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
             return View();
         }
         public IActionResult Show()
         {
+            _memoryCache.Remove("zaman");//bu keye sahip datayı memomry den siler
+
+            //bu keye sahip değeri almaya çalışır eğer yoksa oluşturur
+            _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                entry.AbsoluteExpiration = DateTime.Now.AddSeconds(10); // ekstra işlemler yapmak için var entry
+                return DateTime.Now.ToString();
+            });
+
+
             ViewBag.zaman=_memoryCache.Get<string>("zaman");
             return View();
         }
