20e08800ad0770068f8e3f191384a283f99b2294
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-03-13 16:49:42 +0300

    RegisterPostEvictionCallback

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -27,9 +27,14 @@ namespace InMemoryApp.Web.Controllers
 
             options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); //zaman isminde key oluştuğu zaman burada belirtilen süre kadar cache de kalacak
 
-            options.SlidingExpiration = TimeSpan.FromSeconds(10); //zaman isminde key oluştuğu zaman burada belirtilen süre kadar cache de kullanılmadığı sürece cache de kalacak
+            //options.SlidingExpiration = TimeSpan.FromSeconds(10); //zaman isminde key oluştuğu zaman burada belirtilen süre kadar cache de kullanılmadığı sürece cache de kalacak
 
             options.Priority = CacheItemPriority.High; //cache de yer kalmadığında bu keyi silme önceliği high silme low ilk bunu sil neverremove asla silme normal ise high ile low arasında önceliği var
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) => //cache den silindiğinde (silinme sebebi) çalışacak metot
+            {
+                _memoryCache.Set("callback", $"{key}=>{value}=>sebep:{reason}");
+            });
 
             _memoryCache.Set<string>("zaman", DateTime.Now.ToString(),options);
             //}
@@ -50,6 +55,11 @@ namespace InMemoryApp.Web.Controllers
             //ViewBag.zaman=_memoryCache.Get<string>("zaman");
             
             _memoryCache.TryGetValue("zaman", out string zamancache);
+
+            _memoryCache.TryGetValue("callback", out string callbackcache);
+
+            ViewBag.callback = callbackcache;
+
             ViewBag.zaman = zamancache;
 
             return View();
