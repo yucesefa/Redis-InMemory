0aee532cbb8ca8bb3d381bf2f41ac2e34ca82daf
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-03-13 16:26:14 +0300

    AbsoluteExpiration-SlidingExpiration

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -14,32 +14,42 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
-            //zaman keyi var mı 1.yol
-            if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            ////zaman keyi var mı 1.yol
+            //if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+
             //2.yol
-            if (!_memoryCache.TryGetValue("zaman", out string zamancache))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            //if (!_memoryCache.TryGetValue("zaman", out string zamancache))
+            //{
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+                //options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); //zaman isminde key oluştuğu zaman burada belirtilen süre kadar cache de kalacak
+
+                options.SlidingExpiration = TimeSpan.FromSeconds(10); //zaman isminde key oluştuğu zaman burada belirtilen süre kadar cache de kullanılmadığı sürece cache de kalacak
+
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(),options);
+            //}
 
             return View();
         }
         public IActionResult Show()
         {
-            _memoryCache.Remove("zaman");//bu keye sahip datayı memomry den siler
+            //_memoryCache.Remove("zaman");//bu keye sahip datayı memomry den siler
 
-            //bu keye sahip değeri almaya çalışır eğer yoksa oluşturur
-            _memoryCache.GetOrCreate<string>("zaman", entry =>
-            {
-                entry.AbsoluteExpiration = DateTime.Now.AddSeconds(10); // ekstra işlemler yapmak için var entry
-                return DateTime.Now.ToString();
-            });
+            ////bu keye sahip değeri almaya çalışır eğer yoksa oluşturur
+            //_memoryCache.GetOrCreate<string>("zaman", entry =>
+            //{
+            //    entry.AbsoluteExpiration = DateTime.Now.AddSeconds(10); // ekstra işlemler yapmak için var entry
+            //    return DateTime.Now.ToString();
+            //});
 
+            //ViewBag.zaman=_memoryCache.Get<string>("zaman");
+            
+            _memoryCache.TryGetValue("zaman", out string zamancache);
+            ViewBag.zaman = zamancache;
 
-            ViewBag.zaman=_memoryCache.Get<string>("zaman");
             return View();
         }
     }
