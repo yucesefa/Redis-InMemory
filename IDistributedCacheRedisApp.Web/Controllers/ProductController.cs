71c5bfe8506a834eaa263f5d2af5dba4af80e7c3
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-03-22 02:12:21 +0300

    complex types cache

--- IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -16,13 +19,34 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         {
             DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
             cacheOptions.AbsoluteExpiration=DateTime.Now.AddMinutes(1);
-            await _distributedCache.SetStringAsync("name", "Product 2",cacheOptions);
+
+            Product product = new Product { Id = 1, Name = "Product 1", Price = 100 };
+
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+
+            _distributedCache.Set("product:1", byteProduct, cacheOptions);
+
+            //_distributedCache.SetString("product:1", jsonProduct, cacheOptions);
+
+
+            //await _distributedCache.SetStringAsync("name", "Product 2",cacheOptions);
             return View();
         }
         public IActionResult Show()
         {
-            string name = _distributedCache.GetString("name");
-            ViewBag.name = name;
+            //string name = _distributedCache.GetString("name");
+            //ViewBag.name = name;
+
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+
+            //string jsonProduct = _distributedCache.GetString("product:1");
+
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+            Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            ViewBag.product = product;
+
             return View();
         }
         public IActionResult Remove()
