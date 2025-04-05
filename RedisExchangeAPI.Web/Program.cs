eb778c1b607e4fb01a0f038593e2d26ffd275784
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-05 14:36:08 +0300

    StackExchange.RedisAPI

---
+++ RedisExchangeAPI.Web/Program.cs
@@ -0,0 +1,35 @@
+using RedisExchangeAPI.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<RedisService>(sp=> new RedisService(builder.Configuration));
+
+var app = builder.Build();
+
+RedisService redisService = app.Services.GetRequiredService<RedisService>();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+redisService.Connect();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost; 
+        private readonly string _redisPort;
+        private ConnectionMultiplexer _redisConnection;
+
+        public IDatabase Db { get;  set; }
+
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["RedisHost"];
+            _redisPort = configuration["RedisPort"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+
+            _redisConnection = ConnectionMultiplexer.Connect(configString); 
+        }
+        
+        public IDatabase GetDb(int db)
+        {
+            return _redisConnection.GetDatabase(db);
+        }
+    }
+}
