cdf29b3153f1ae7b06bb7d7c7297dcb478eb9c1c
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-15 23:02:55 +0300

    inmemory redis service and cache structure

---
+++ RedisExampleApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repository;
+using RedisExchangeAPI.Web.Services;
+
+namespace RedisExampleApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly RedisService _redisService;
+
+        public ProductsController(IProductRepository productRepository, RedisService redisService)
+        {
+            _productRepository = productRepository;
+            _redisService = redisService;
+
+            //var db = _redisService.GetDatabase(0);
+            //db.StringSet("test", "Hello from Redis");
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _productRepository.GetAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _productRepository.GetByIdAsync(id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            return Created(string.Empty, await _productRepository.CreateAsync(product));
+        }
+    }
+}
---
+++ RedisExampleApp.API/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repository;
+using RedisExchangeAPI.Web.Services;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("InMemoryDb");
+});
+
+//builder.Services.AddSingleton<RedisService>(sp=>
+//{
+//    return new RedisService(builder.Configuration["CacheOptions:Url"]);
+//})
+
+//builder.Services.AddSingleton<IDatabase>(sp =>
+//{
+//    var redisService=sp.GetRequiredService<RedisService>();
+//    return redisService.GetDatabase(0);
+//})
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
